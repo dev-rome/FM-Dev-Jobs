a1b8339446e34c877eeb08eb46fed4629f7148cb
dev-rome <info.devrome@gmail.com>
2024-07-21 12:17:43 -0400

    add toggle switch

---
+++ components/Navigation.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import React, { useState } from "react";
+import Image from "next/image";
+
+import Logo from "../public/assets/desktop/logo.svg";
+import Sun from "../public/assets/desktop/icon-sun.svg";
+import Moon from "../public/assets/desktop/icon-moon.svg";
+
+const Navigation = () => {
+  const [isToggled, setIsToggled] = useState(false);
+
+  const handleIsToggled = () => {
+    setIsToggled(!isToggled);
+  };
+
+  return (
+    <nav className="relative flex items-center bg-header-pattern-desktop bg-cover bg-no-repeat">
+      <Image src={Logo} alt="devjobs" />
+      <div className="flex items-center gap-4">
+        <Image src={Sun} alt="sun" />
+        <label className="flex items-center">
+          <div className="relative">
+            <input
+              type="checkbox"
+              checked={isToggled}
+              onChange={handleIsToggled}
+              className="sr-only"
+            />
+            <div className="block h-6 w-12 rounded-full bg-white"></div>
+            <span
+              className={`bg-royal-blue absolute left-1 top-1 h-4 w-4 transform rounded-full transition-transform ${
+                isToggled ? "translate-x-6" : ""
+              }`}
+            ></span>
+          </div>
+        </label>
+        <Image src={Moon} alt="moon" />
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
