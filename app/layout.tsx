f2e938bcb1069303cd115d022c3148c649b1bded
dev-rome <info.devrome@gmail.com>
2024-07-21 12:16:26 -0400

    import navigation component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Kumbh_Sans } from "next/font/google";
 import "./globals.css";
+import Navigation from "@/components/Navigation";
 
 const kumbhSans = Kumbh_Sans({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={kumbhSans.className}>
+        <Navigation />
         <main>{children}</main>
       </body>
     </html>
